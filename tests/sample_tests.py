6155ab881d75a2574b1f42489d7a7d6a6049bdac
emd <emd@mit.edu>
2015-10-07 18:34:01 -0700

    Combined all ValueError tests in `sample.decimate(...)`

--- tests/sample_tests.py
+++ tests/sample_tests.py
@@ -2,13 +2,19 @@ from nose import tools
 from digidemod.sample import decimate
 
 
-@tools.raises(ValueError)
-def test_decimate_valueError1():
+def test_decimate_valueErrors():
     # Attempts to decimate by a *negative* value should fail
-    decimate(1, -1)
+    tools.assert_raises(ValueError, decimate, 1, -1)
 
-
-@tools.raises(ValueError)
-def test_decimate_valueError2():
     # Attempts to decimate by anything other than an int should fail
-    decimate(1, 1.5)
+    tools.assert_raises(ValueError, decimate, 1, 1.5)
+
+    # The transition band of the low pass filter cannot occupy
+    # a negative fraction of the filtered signal's spectral bandwidth
+    tools.assert_raises(ValueError, decimate,
+                        1, 10, {'trans': -0.1})
+
+    # The transition band of the low pass filter cannot occupy
+    # a bandwidth larger than that of the filtered signal's bandwidth
+    tools.assert_raises(ValueError, decimate,
+                        1, 10, {'trans': 1.1})
