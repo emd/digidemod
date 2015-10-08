32bd61d2f6622a287c8b95619d68f366f35e6ef1
emd <emd@mit.edu>
2015-10-07 19:30:52 -0700

    Decimation function (roughly) completed.
    Past tests updated to remain compatible with new functionality
    of deceimate(...).

--- digidemod/sample.py
+++ digidemod/sample.py
@@ -1,12 +1,24 @@
 'Provides routines for (re)sampling and or decimating a signal.'
 
 
-import numpy as np
 from filters.fir import NER
 
 
-def decimate(y, Q):
+def decimate(y, Q, t=None, trans=0.5, ripple=0.02):
     '''Decimate signal `y` by integer factor `Q`.
+
+    The decimation is performed by passing the signal `y` through a
+    Nearly Equal Ripple (NER) low pass filter and then slicing.
+    NER is an FIR filtering technique that allows the user to specify
+    the maximum deviation in the passband and stopband ripple. Further,
+    the NER filter employed here uses an *odd* number of taps;
+    it is a general result that an FIR filter with an odd number of taps
+    is "zero phase" - that is, it introduces *no* phase delay between
+    the input signal and the resulting filtered signal.
+
+    The -3 dB cutoff frequency of the NER low pass filter is specified
+    such that the Nyquist frequency of the decimated signal lies
+    at the boundary between the transition band and stopband.
 
     Parameters:
     -----------
@@ -18,14 +30,80 @@ def decimate(y, Q):
         The decimation factor. Q must be a positive integer;
         a ValueError is raised otherwise.
 
+    t - array_like, (`N`,)
+        The time base corresponding to input signal `y`.
+        If `t` is provided, a new time base corresponding
+        to the decimated signal will additionally be returned.
+        [t] = arbitrary
+
+    trans - float
+        The fraction of the decimated signal's spectrum in the
+        transition band. To ensure a finite passband, 0 < trans < 1,
+        with values outside of this bound raising a ValueError.
+        [trans] = unitless
+
+    ripple - float
+        The maximum deviation in the passband and stopband ripple.
+        [ripple] = unitless
+
     Returns:
     --------
-    ydec - array_like, (`N`)
+    Tuple (`tdec`, `ydec`, `valid`) or (`ydec`, `valid`) where
+
+    tdec - array_like, ((`N` // Q) + 1,)
+        The decimated signal's time base. This will *only* be returned
+        if the original signal's time base is provided in the function call.
+        [tdec] = [t]
+
+    ydec - array_like, ((`N` // Q) + 1,)
         The decimated signal.
         [ydec] = [y]
+
+    valid - slice
+        The slice corresponding to "valid" indices of `tdec` and `ydec`.
+        The low pass filtering involves a convolution, so boundary
+        effects are visible at the edges of `tdec` and `ydec`.
+        However, boundary effects are *not* present for the slices
+
+                    `tdec[valid]` and `ydec[valid]`
+
+        where `valid` corresponds to convolution with mode = 'valid'.
 
     '''
     if (Q < 1) or not isinstance(Q, int):
         raise ValueError('`Q` must be a positive integer.')
 
-    pass
+    if (trans < 0) or (trans > 1):
+        raise ValueError('`trans` must be between 0 and 1.')
+
+    if (t is not None) and (len(t) != len(y)):
+        raise ValueError('`t` must have same dimensions as `y`.')
+
+    # We require that the Nyquist frequency of the *decimated* signal f'_Ny
+    # is at the boundary between the filter's transition band and stopband,
+    # from which we can easily derive the following formula for the
+    # filter's -3 dB cutoff frequency.
+    fc = 1 - (0.5 * trans)
+
+    # Create low pass filter. Note that `fc` and `trans` are normalized
+    # to the Nyquist frequency f'_Ny of the decimated signal, and that the
+    # sampling frequency Fs of the original signal `y` can be expressed as
+    #
+    #                       Fs / f'_Ny = 2 * Q
+    #
+    lpf = NER(ripple, [fc], trans, 2 * Q, pass_zero=True)
+
+    # Filter and decimate signal
+    ydec = lpf.applyTo(y)[::Q]
+
+    # However, boundary effects will be visible in the edges of `ydec`.
+    # Let's find the corresponding "valid" indices where boundary effects
+    # are *not* present.
+    valid = lpf.getValidSlice()
+    valid.start = (valid.start // Q) + 1
+    valid.stop = (valid.stop // Q) - 1
+
+    if t is None:
+        return ydec, valid
+    else:
+        return t[::Q], ydec, valid
--- tests/sample_tests.py
+++ tests/sample_tests.py
@@ -1,20 +1,25 @@
 from nose import tools
+import numpy as np
 from digidemod.sample import decimate
 
 
 def test_decimate_valueErrors():
     # Attempts to decimate by a *negative* value should fail
-    tools.assert_raises(ValueError, decimate, 1, -1)
+    tools.assert_raises(ValueError, decimate, np.array([1]), -1)
 
     # Attempts to decimate by anything other than an int should fail
-    tools.assert_raises(ValueError, decimate, 1, 1.5)
+    tools.assert_raises(ValueError, decimate, np.array([1]), 1.5)
 
     # The transition band of the low pass filter cannot occupy
     # a negative fraction of the filtered signal's spectral bandwidth
     tools.assert_raises(ValueError, decimate,
-                        1, 10, {'trans': -0.1})
+                        np.array([1]), 10, {'trans': -0.1})
 
     # The transition band of the low pass filter cannot occupy
     # a bandwidth larger than that of the filtered signal's bandwidth
     tools.assert_raises(ValueError, decimate,
-                        1, 10, {'trans': 1.1})
+                        np.array([1]), 10, {'trans': 1.1})
+
+    # (Optional) time base and signal must have same length
+    tools.assert_raises(ValueError, decimate,
+                        np.zeros(10), 10, {'t': np.zeros(9)})
